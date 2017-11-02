611be05df0e22475d232b3d932413a1e193593b8
Kieu Tam <kvttam@gmail.com>
2017-11-02 23:04:44 +0700

    update 02-11-2017

--- js/main.js
+++ js/main.js
@@ -13,3 +13,37 @@ $("div#Main-slide").owlCarousel({
     pagination: false,
     navigationText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
 });
+$(".btn-menu").click(function(){
+    $(".navabar_HD #menu").toggleClass("show");
+});
+$(document).ready(function () {
+    if($(window).width() < 543) {
+        $('.navabar_HD #menu').slideAndSwipe();
+    }
+    $(".soluong .btnTang").on("click", function(){
+        var numbSL = $(this).parent().find(".numbSL").text();
+        var max = $(this).parent().find(".numbSL").attr("max");
+        if(numbSL < max) {
+            $(this).parent().find(".numbSL").html(parseInt(numbSL)+1);
+        }
+    });
+    $(".soluong .btnGiam").on("click", function(){
+        var numbSL = $(this).parent().find(".numbSL").text();
+        if(numbSL>0) {
+            $(this).parent().find(".numbSL").html(parseInt(numbSL)-1);
+        }
+        if($(this).closest(".listPr").find(".selectProduct").hasClass("selected") == true) {
+            var numbSL = $(this).parent().find(".numbSL").text();
+            if(numbSL == 0) {
+                $(this).parent().find(".numbSL").html("1");
+            }
+        }
+    });
+    $(".selectProduct").on("click", function(){
+        $(this).toggleClass("selected");
+        var numbSL = $(this).closest(".listPr").find(".soluong .numbSL").text();
+        if(numbSL == 0 && $(this).hasClass("selected") == true) {
+            $(this).closest(".listPr").find(".soluong .numbSL").html("1");
+        }
+    });
+})
